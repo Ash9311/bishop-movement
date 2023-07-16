a146b49c49683dbb0d16adff09bc4f95fa662dc2
@Ash9311 <ashutoshmulki7@gmail.com>
2023-07-17 00:22:40 +0530

    skeleton chess board

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 const chessBoard = document.querySelector('.chess-board');
 
-for(let i=0;i<8;i++){
 
+renderCB();
+
+
+function renderCB(){
+for(let i=0;i<8;i++){
+ const row = document.createElement('div');
+ row.classList.add('row');
+
+ for(let j=0;j<8;j++){
+    const square = document.createElement('div');
+    square.classList.add('square');
+
+    if((i+j)%2==0){
+        square.classList.add('white');
+    }
+    else{
+        square.classList.add('black');
+    }
+    row.appendChild(square);
+ }
+
+
+ chessBoard.appendChild(row);
+}
 }
