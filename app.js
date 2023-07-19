a64e8462abc6c6b6ccd663f6683b55ae8810d57a
@Ash9311 <ashutoshmulki7@gmail.com>
2023-07-19 23:55:44 +0530

    implementation

--- app.js
+++ app.js
@@ -1,28 +1,58 @@
 const chessBoard = document.querySelector('.chess-board');
 
+const squares = document.querySelectorAll('.square');
 
 renderCB();
 
 
-function renderCB(){
-for(let i=0;i<8;i++){
- const row = document.createElement('div');
- row.classList.add('row');
+function renderCB() {
+    for (let i = 0; i < 8; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
 
- for(let j=0;j<8;j++){
-    const square = document.createElement('div');
-    square.classList.add('square');
+        for (let j = 0; j < 8; j++) {
+            const square = document.createElement('div');
+            square.classList.add('square');
+            
+    square.addEventListener('mouseover', () => {
+        //remove dark blue class from all sqaures
+        squares.forEach(square => square.classList.remove('dark-blue'));
 
-    if((i+j)%2==0){
-        square.classList.add('white');
+        //add light blue class to the hovered square
+        square.classList.add('light-blue');
+
+
+        //get row and col index of the hovered square
+        const rowIndex = Array.from(square.parentNode.children).indexOf(square);
+        const colIndex = Array.from(square.parentNode.parentNode.children).indexOf(square.parentNode);
+
+        //highlight diagonally attacked squares
+        for (let i = 0; i < 8; i++) {
+            const row = document.querySelectorAll('.row')[i];
+            const squaresInRow = row.children;
+
+            for (let j = 0; j < 8; j++) {
+                const currentSquare = squaresInRow[j];
+                const currentRowIndex = Array.from(currentSquare.parentNode.children).indexOf(currentSquare);
+                const currentColIndex = Array.from(currentSquare.parentNode.parentNode.children).indexOf(currentSquare.parentNode);
+                if (Math.abs(currentRowIndex - rowIndex) === Math.abs(currentColIndex - colIndex) && square != currentSquare) {
+                    currentSquare.classList.add('dark-blue');
+                }
+
+            }
+        }
+
+
+    square.addEventListener('mouseout', () => {
+        squares.forEach(square => square.classList.remove('light-blue', 'dark-blue'));
+    });
+});
+
+
+            row.appendChild(square);
+        }
+
+
+        chessBoard.appendChild(row);
     }
-    else{
-        square.classList.add('black');
-    }
-    row.appendChild(square);
- }
-
-
- chessBoard.appendChild(row);
-}
 }
